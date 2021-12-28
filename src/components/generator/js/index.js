12dad8bc33f38e4194fbb9cd55d077870f3b41fa
kongliyuan <kongliyuan@360.cn>
2021-12-28 15:05:59 +0800

    chore: test

--- src/components/generator/js/index.js
+++ src/components/generator/js/index.js
@@ -152,8 +152,8 @@ function buildAttributes(params) {
 // 在Created调用函数
 function callInCreated(methodName, created) {
   created.push(`this.${methodName}()`)
+  console.log(8)
   console.log(99)
-  console.log(799997)
 }
 
 // 混入处理函数
